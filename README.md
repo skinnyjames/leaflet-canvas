fe81be057a4fc4045dce54be3e1d0c565c530d37
Sean Gregory <seangregory@protonmail.com>
2018-03-28 14:00:09 -0500

    dist is good

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # leaflet-canvas
+
+a canvas layer for leaflet maps
+dervived from [Sumbera/gLayers.Leaflet](http://github.com/Sumbera/gLayers.Leaflet)
+
+
+## test it out
+
+```
+git clone git@github.com:skinnyjames/leaflet-canvas.git
+npm install 
+npm run build
+npm run dev
+
+```
--- leaflet-canvas.js
+++ leaflet-canvas.js
@@ -4,7 +4,7 @@ L.CanvasLayer = (L.Layer ? L.Layer : L.Class).extend({
   options: {},
 
   // Public Methods
-  initialize(opts) {
+  initialize(options) {
 
     this._map    = null
     this._canvas = null
@@ -21,6 +21,7 @@ L.CanvasLayer = (L.Layer ? L.Layer : L.Class).extend({
   onAdd(map) {
     this._map = map
     let size = this._map.getSize()
+    let self = this
 
     this._canvas = L.DomUtil.create('canvas', 'leaflet-layer')
     this._canvas.width = size.x
@@ -30,11 +31,8 @@ L.CanvasLayer = (L.Layer ? L.Layer : L.Class).extend({
     L.DomUtil.addClass(this._canvas, 'leaflet-zoom-' + (animated ? 'animated' : 'hide'))
 
     this._map._panes.overlayPane.appendChild(this._canvas)
-
-    this._map.on(this._getEvents(), this)
     
     this.fire('mounted', {map: this._map, canvas: this._canvas})  
-    
     this.draw()
   },
 
@@ -48,6 +46,7 @@ L.CanvasLayer = (L.Layer ? L.Layer : L.Class).extend({
 
   draw() {
     if (!this._frame) {
+      console.log('requesting animation frame')
       this._frame = L.Util.requestAnimFrame(this.drawLayer, this)
     }
     return this;
@@ -76,22 +75,45 @@ L.CanvasLayer = (L.Layer ? L.Layer : L.Class).extend({
     this._frame = null;
   },
 
-  // Private Methods
-  _getEvents() {
+  getEvents() {
 
-    return {
+    let self = this
+    var lastClick = null
+
+    let events = {
       click:     dispatch('click'),
+      move:      dispatch('move'),
       moveend:   dispatch('moveend'),
       viewreset: dispatch('viewreset'),
       resize:    dispatch('resize'),
+      zoom:      dispatch('zoom'),
+      mousemove: dispatch('mousemove'),
     }
+
+    if (this._map.options.zoomAnimation && L.Browser.any3d) {
+        events.zoomanim =  this._animateZoom
+    }
+
+    return events
     
-    let dispatch = function(eventName) {
-      let self = this
+    function dispatch(eventName) {
 
       return function(e) {
-        e.prototype.getBufferedBounds = getBufferedBounds
+        if (eventName == 'moveend') {
+          if (self._canvas) {
+            var topLeft = self._map.containerPointToLayerPoint([0, 0])
+            L.DomUtil.setPosition(self._canvas, topLeft)
+          }
+          self.draw()
+        }
+
+        if (eventName == 'click' || eventName == 'mousemove') {
+          e.getBufferedBounds = getBufferedBounds
+        } 
+
+        e.canvas = self._canvas  
         self.fire(eventName, e)
+
       }
     }
 
@@ -106,8 +128,8 @@ L.CanvasLayer = (L.Layer ? L.Layer : L.Class).extend({
         y: this.containerPoint.y + pixels
       }
 
-      let leftcorner = this.target.containerPointToLatLng(L.point(bufferUpperLeft))
-      let rightcorner = this.target.containerPointToLatLng(L.point(bufferLowerRight))
+      let leftcorner =  self._map.containerPointToLatLng(L.point(bufferUpperLeft))
+      let rightcorner = self._map.containerPointToLatLng(L.point(bufferLowerRight))
 
       return L.latLngBounds(leftcorner, rightcorner)   
     }
@@ -118,5 +140,32 @@ L.CanvasLayer = (L.Layer ? L.Layer : L.Class).extend({
       x: latlon.lng * 6378137 * Math.PI / 180,
       y: Math.log(Math.tan((90 + latlon.lat) * Math.PI / 360)) * 6378137
     }
-  }
+  },
+
+  _setTransform: function (el, offset, scale) {
+
+    var pos = offset || new L.Point(0, 0);
+    var translate
+
+    if (L.Browser.ie3d) {
+      translate = 'translate(' + pos.x + 'px,' + pos.y + 'px)'
+    } else {
+      translate = 'translate3d(' + pos.x + 'px,' + pos.y + 'px,0)' + (scale ? ' scale(' + scale + ')' : '');
+    }
+    
+    el.style[L.DomUtil.TRANSFORM] = translate
+  },
+
+  _animateZoom: function (e) {
+
+      var scale = this._map.getZoomScale(e.zoom);
+      var offset
+      // -- different calc of animation zoom  in leaflet 1.0.3 thanks @peterkarabinovic, @jduggan1 
+      if (L.Layer) {
+        offset = this._map._latLngBoundsToNewLayerBounds(this._map.getBounds(), e.zoom, e.center).min 
+      } else {
+        offset = this._map._getCenterOffset(e.center)._multiplyBy(-scale).subtract(this._map._getMapPanePos());
+      }
+      L.DomUtil.setTransform(this._canvas, offset, scale);
+  },
 })
